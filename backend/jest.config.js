9be3d866019363060f8dc29c5d3d65398dc6b537
Mark Wayne Menorca <marcuwynu23@gmail.com>
2026-05-04 16:04:21 +0800

    chore: add jest for testing

---
+++ backend/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.test.ts"],
+
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+
+  clearMocks: true,
+  verbose: true,
+};
